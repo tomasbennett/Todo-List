926978b84ba4592c7d1558ae991e2076b617bf8e
tomasbennett <tjs.bennett@gmail.com>
2025-05-07 23:56:40 +0100

    Adding in the main html skeleton as well as the css files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import "./Pages/HTML/index.html";
 
 import "./Pages/CSS/style.css";
+import "./Pages/CSS/Main/main.css";
+import "./Pages/CSS/Main/aside.css";
+import "./Pages/CSS/Main/outer-card.css";
+
 
 import "./TypeScript/Main.ts";
