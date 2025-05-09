6ba118732bae7b4cb4e3bfa6f39af392e617b972
tomasbennett <tjs.bennett@gmail.com>
2025-05-09 16:55:07 +0100

    Adding an add-button button element to the document as well as a stylesheet for a future dialog element set to add Tasks, Projects and Notes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./Pages/CSS/style.css";
 import "./Pages/CSS/Main/main.css";
 import "./Pages/CSS/Main/aside.css";
 import "./Pages/CSS/Main/outer-card.css";
+import "./Pages/CSS/Main/dialog-box.css";
 
 
 import "./TypeScript/Main.ts";
