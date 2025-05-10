d1652e86a450a08812be023be39020c6eab2e9ef
tomasbennett <tjs.bennett@gmail.com>
2025-05-11 00:37:59 +0100

    Adding input boxes and a form element to the dialog box for the construction of new todo list items

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./Pages/CSS/style.css";
 import "./Pages/CSS/Main/main.css";
 import "./Pages/CSS/Main/aside.css";
 import "./Pages/CSS/Main/outer-card.css";
-import "./Pages/CSS/Main/dialog-box.css";
-
+import "./Pages/CSS/DialogBox/dialog-box.css";
+import "./Pages/CSS/DialogBox/dialog-form.css";
 
 import "./TypeScript/Main.ts";
