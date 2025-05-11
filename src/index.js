70c6be6efadb71a11e658656c723fe67d33b703d
tomasbennett <tjs.bennett@gmail.com>
2025-05-11 15:10:34 +0100

    Completing all three forms in html inside the dialog box with an over use of attributes and css that I hope to fix in future projects

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import "./Pages/CSS/Main/main.css";
 import "./Pages/CSS/Main/aside.css";
 import "./Pages/CSS/Main/outer-card.css";
 import "./Pages/CSS/DialogBox/dialog-box.css";
-import "./Pages/CSS/DialogBox/dialog-form.css";
+import "./Pages/CSS/DialogBox/dialog-task-form.css";
+import "./Pages/CSS/DialogBox/dialog-project-form.css";
+import "./Pages/CSS/DialogBox/dialog-note-form.css";
 
 import "./TypeScript/Main.ts";
